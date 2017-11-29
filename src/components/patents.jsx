8ca6429cb2a28a768f209505d022419b0a0be6d8
Soo-Rae Hong <the.soorae@gmail.com>
2017-11-29 00:19:19 -0800

    finish patent with image toggle

--- src/components/patents.jsx
+++ src/components/patents.jsx
@@ -6,42 +6,99 @@ class Patents extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-      current_patent: ""
+      idx: ""
     }
     this.render_patent_block = this.render_patent_block.bind(this)
   }
 
   render_related_patents(patent){
-    if (patent.related){
-      patent.related.map((rel) => {
+    console.log("patent related", patent.related)
+    if (patent.related && patent.related.length > 0 ){
+      let related_links = patent.related.map((rel) => {
         return (
           <a className="patent_link" href={rel.link}>
             {rel.title}
           </a>
         )
       })
+      return (
+        <div className="related_links">
+          <h3 className="related_title">
+            Related Patents
+          </h3>
+          {related_links}
+        </div>
+      )
     }
     
   }
 
-  render_patent_block(patent){
+  toggle_images(idx){
+    return (e) => {
+      e.preventDefault()
+      if (this.state.idx === idx) {
+        this.setState({
+          idx: ""
+        })
+      } else {
+        this.setState({
+          idx: idx
+        })
+      }
+    }
+  }
+
+  render_view_images(patent, idx){
+    if (patent.images && patent.images.length > 0 ){
+      let button_text = "View Images"
+      let arrow = "down"
+      if (this.state.idx === idx) {
+        button_text = "Hide Images"
+        arrow = "up"
+      }
+      return (
+        <div className="image_toggle" onClick={this.toggle_images(idx)}>
+          <div className={`arrow ${arrow}`}/>
+          {button_text}
+        </div>
+      )
+    }
+  }
+
+  render_images(patent, idx){
+    if (this.state.idx === idx) {
+      return (
+        <div className="images">
+          {patent.images.map(img => {
+            return (
+              <img className="patent_image" src={img} />
+            )}
+            )}
+        </div>
+      )
+    }
+  }
+  render_patent_block(patent, idx){
     return (
       <a className="patent_block" href={patent.link}>
         <div className="num_row">
-          <div className="patent_num">{patent.number}</div>
+          <h2 className="patent_num">{patent.number}</h2>
           <div className="patent_date">{patent.date}</div>
         </div>
         <div className="patent_title">{patent.title}</div>
         <div className="patent_desc">{patent.abstract}</div>
-        <div className="related_patents">
+        <div className="patent_footer">
           {this.render_related_patents(patent)}
+          {this.render_view_images(patent, idx)}
         </div>
+        {this.render_images(patent, idx)}
+
       </a>
     )
   }
 
   render(){
-    let patent_blocks = PATENTS.map(patent => this.render_patent_block(patent))
+    let patent_blocks = PATENTS.map((patent, idx) => this.render_patent_block(patent, idx))
     return(
       <div className="patents">
         {patent_blocks}
