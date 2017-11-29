8da408797a471b299f326ac566c80cb94de875ea
Soo-Rae Hong <the.soorae@gmail.com>
2017-11-29 00:25:08 -0800

    move technologies css

--- src/components/technologies.jsx
+++ src/components/technologies.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {categories} from '../data';
+import {CATEGORIES} from '../data';
 import CategoryDetail from './category_detail';
-import './technologies.css';
+import '../css/technologies.css';
 
 class Technologies extends React.Component{
   render(){
-    const categoryLinks = categories.map(category => (<div className="category">{category.title}</div>));
-    const categoryDetail = categories.map(category =>(<CategoryDetail cat={category}/>))
+    const categoryLinks = CATEGORIES.map(category => (<div className="category">{category.title}</div>));
+    const categoryDetail = CATEGORIES.map(category =>(<CategoryDetail cat={category}/>))
     return(
       <div className="technologies">
         <div className="cat-links">
